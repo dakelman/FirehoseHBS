e3a53f1b144dcbd650cdeaf55545e291016724ca
Diana Akelman <dakelman@dakelman-VirtualBox.(none)>
2013-11-16 17:23:36 -0500

    added new entries and controller

--- app/controllers/breeders_controller.rb
+++ app/controllers/breeders_controller.rb
@@ -6,4 +6,22 @@ class BreedersController < ApplicationController
 
 	end
 
+	def new
+
+		@breeders = Breeders.new
+
+	end
+
+	def create
+		@breeders = Breeders.create( breeders_params )
+		redirect_to breeders_path
+	end
+
+
+	private
+
+	def breeders_params 
+		params.require(:breeders).permit(:breed, :about_me, :age)
+	end
+
 end
